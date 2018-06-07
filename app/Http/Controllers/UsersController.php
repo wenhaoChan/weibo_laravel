26b370fb17ac02f3ccecebe9734a6c4093c970aa
chenwenhao <zerochan1220@163.com>
2018-06-07 16:05:02 +0800

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -101,13 +101,13 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'zerochan0226@gmail.com';
-        $name = 'zero';
+//        $from = 'zerochan0226@gmail.com';
+//        $name = 'zero';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
